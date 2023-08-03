0696b3e51fb2229ae1cb64246d01700a08cf9528
Ajarek <ajarek@poczta.onet.pl>
2023-08-04 00:46:41 +0200

    initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+     <h2>Ball</h2>
+     <img src="/images/kula.png" alt="" />
+    </div>
+  )
+}
+
+export default Home
